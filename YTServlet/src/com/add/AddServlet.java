5064ba69a3fdb93d2a5b204fa8ca710754249c77
Yash Vadhadiya <Yash Vadhadiya@DESKTOP-0GR86PJ>
2021-02-26 22:45:31 +0530

    Updated

---
+++ YTServlet/src/com/add/AddServlet.java
@@ -0,0 +1,21 @@
+package com.add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i+j;
+		PrintWriter out=res.getWriter();
+		out.println("result is: " +k);
+		
+		
+	}
+
+}
